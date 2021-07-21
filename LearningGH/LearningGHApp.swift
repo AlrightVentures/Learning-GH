a863a6bf822419164e91030a8f5a7b0f2ac7d978
Zatarra <58144826+AlrightVentures@users.noreply.github.com>
2021-07-21 19:37:52 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Bart Baranowski on 21/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
